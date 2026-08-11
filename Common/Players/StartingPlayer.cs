e2da6057a6a4d2949f4d00167a1ca869546f4747
intelligentbed <dankmemelord992@gmail.com>
2026-08-11 17:45:13 -0400

    Add files via upload

---
+++ Common/Players/StartingPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ShatteredIllusion.Common.Players
+{
+	public class StartingPlayer : ModPlayer
+	{
+		public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath)
+		{
+			if (!mediumCoreDeath)
+			{
+				yield return new Item(ModContent.ItemType<StarterBag>());
+			}
+		}
+	}
+}
