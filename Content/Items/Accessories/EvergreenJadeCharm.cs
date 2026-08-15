50b25432c71e51e1e972d00f03e1a3a5835cef42
intelligentbed <dankmemelord992@gmail.com>
2026-08-15 10:34:55 -0400

    Refactor EvergreenJadeCharm class and methods

--- Content/Items/Accessories/EvergreenJadeCharm.cs
+++ Content/Items/Accessories/EvergreenJadeCharm.cs
@@ -54,7 +54,7 @@ namespace ShatteredIllusion.Content.Items.Accessories
             }
         }
 
-        // Triggers when hitting an enemy with tmelee 
+        // avtivates when hitting an enemy with tmelee 
         public override void OnHitNPCWithItem(Item item, NPC target, NPC.HitInfo hit, int damageDone)
         {
             if (hasEvergreenJadeCharm && item.CountsAsClass(DamageClass.Melee))
@@ -63,10 +63,9 @@ namespace ShatteredIllusion.Content.Items.Accessories
             }
         }
 
-        // Triggers when hitting an enemy with a projectile
+        // same as the comment above but with projectile
         public override void OnHitNPCWithProj(Projectile proj, NPC target, NPC.HitInfo hit, int damageDone)
         {
-            // Avoidd loops and while only making it spawn with melee projectiles
             if (hasEvergreenJadeCharm && proj.type != ModContent.ProjectileType<JadeSprout>() && proj.CountsAsClass(DamageClass.Melee))
             {
                 TrySpawnJadeSprout(proj.GetSource_FromThis());
@@ -78,10 +77,10 @@ namespace ShatteredIllusion.Content.Items.Accessories
             // the chance to spawn a Jade Sprout is 1 in 2 so 50% chance
             if (Main.rand.NextBool(2) && Player.whoAmI == Main.myPlayer)
             {
-                // Spawns at player center then goes in the direction of the cursor position
+   
                 Vector2 direction = Vector2.Normalize(Main.MouseWorld - Player.Center);
 
-                // Set initial toss speed
+                // Set speed
                 Vector2 velocity = direction * 26f;
 
                 Projectile.NewProjectile(
