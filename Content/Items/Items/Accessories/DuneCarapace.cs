ecd149b3f65ee51d9d8f1722810405245e8dd46e
intelligentbed <dankmemelord992@gmail.com>
2026-08-27 19:52:49 -0400

    WORKING ON THE PILE

---
+++ Content/Items/Items/Accessories/DuneCarapace.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShatteredIllusion.Content.Items.Accessories
+{
+    internal class DuneCarapace : ModItem
+    {
+        private const int BaseDefense = 4; 
+        private const int BonusDefense = 8;
+        private const float HealthThreshold = 0.25f; 
+
+        public override void SetDefaults()
+        {
+            Item.width = 26;
+            Item.height = 26;
+            Item.accessory = true;
+
+            Item.value = Item.sellPrice(gold: 5);
+            Item.rare = ItemRarityID.LightRed;
+            Item.defense = BaseDefense;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            float lifeRatio = (float)player.statLife / player.statLifeMax2;
+
+            if (lifeRatio < HealthThreshold)
+            {
+                float scale = 1f - (lifeRatio / HealthThreshold); 
+                int bonus = (int)System.Math.Round(BonusDefense * scale);
+                player.statDefense += bonus;
+            }
+        }
+
+    }
+}
---
+++ Content/Items/Tiles/Trophys/AntlionTrophy.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ShatteredIllusion.Content.Tiles.Trophys
+{
+    public class AntlionTrophy : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.addTile(Type);
+
+            DustType = DustID.WoodFurniture;
+        }
+    }
+    public class AntlionTrophyItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<AntlionTrophy>());
+
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.rare = ItemRarityID.Blue;
+        }
+    }
+}
