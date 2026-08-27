43eb41b82ed3643de1e46fda68173b3f89348e1a
intelligentbed <dankmemelord992@gmail.com>
2026-08-27 19:56:36 -0400

    Delete Content/Items/Tiles directory

--- Content/Items/Tiles/Relics/AntlionRelic.cs
+++
@@ -1,9 +0,0 @@
-﻿using Terraria.ModLoader;
-
-namespace ShatteredIllusion.Content.Tiles.Relics
-{
-    public class AntlionRelic : RelicBase
-    {
-        protected override int ItemType => ModContent.ItemType<Items.Placeables.Relics.AntlionRelic>();
-    }
-}
