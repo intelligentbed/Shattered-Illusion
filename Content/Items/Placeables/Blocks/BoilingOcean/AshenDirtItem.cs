b90e67dc2bb0c50b88af5b4c926a75ccbfc02912
intelligentbed <dankmemelord992@gmail.com>
2026-09-11 22:17:42 -0400

    Add files via upload

---
+++ Content/Items/Placeables/Blocks/BoilingOcean/AshenDirtItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace ShatteredIllusion.Content.Items.Placeables.Blocks.BoilingOcean
+{
+    public class AshenDirtItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.useTime = 10;
+            Item.useAnimation = 15;
+            Item.useStyle = Terraria.ID.ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = 0;
+            Item.createTile = ModContent.TileType<Content.Tiles.BoilingOcean.AshenDirt>();
+        }   
+    }
+}
---
+++ Content/Items/Placeables/Blocks/BoilingOcean/AshenSandItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.ObjectData;
+using ShatteredIllusion.Content.Tiles.BoilingOcean;
+
+namespace ShatteredIllusion.Content.Items.Placeables.Blocks.BoilingOcean
+{
+    public class AshenSandItem : ModItem
+    {
+        public override void SetDefaults() 
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.useTime = 10;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = 0;
+            Item.createTile = ModContent.TileType<AshenSand>();
+        }
+    }
+}
